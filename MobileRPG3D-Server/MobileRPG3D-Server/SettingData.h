4efe6b505dcc64590c206ce341c29693773095d0
Reoul <dydqja1358@gmail.com>
2022-09-10 21:51:25 +0900

    수정
    수정

---
+++ MobileRPG3D-Server/MobileRPG3D-Server/SettingData.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+constexpr int MAX_ROOM_PLAYER = 8;
+constexpr int MAX_BUF_SIZE = 1300;
+constexpr int MAX_PACKET_SIZE = 1300;
+constexpr int MAX_USER = 5000;
+constexpr int SERVER_PORT = 51234;
+constexpr int MAX_USER_NAME_LENGTH = 11;
+constexpr int MAX_USING_ITEM = 6;
+constexpr int MAX_UN_USING_ITEM = 8;
